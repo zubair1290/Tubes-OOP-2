f22ad9211e686bfe9fa66471dad78b3bd2edc805
raffifpr <68497910+raffifpr@users.noreply.github.com>
2021-04-22 13:44:50 +0700

    Update Skill.java

--- src/Skill.java
+++ src/Skill.java
@@ -2,6 +2,23 @@
  * Skill
  */
 public class Skill {
-
-    
+	protected int basePower;
+	protected int masteryLevel;
+	protected String skill;
+	List <Element> elements;
+	protected int jumlahElements;
+	
+	public Skill(String s, int b, int m, Element e, int je) {
+		this.basePower = b;
+		this.masteryLevel = m;
+		this.skill = s;
+		this.elements = new ArrayList<Element>(je);
+		this.jumlahElements = je;
+	}
+	public int getPower(){
+		return this.basePower;
+	}
+	public List<Element> getElements(){
+		return this.elements;
+	}   
 }
